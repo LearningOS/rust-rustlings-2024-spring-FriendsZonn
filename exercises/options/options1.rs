96b40e5bb57ec59d360936a44869389ca9e75854
FriendsZonn <1305887195@qq.com>
2024-05-30 01:08:53 +0800

    2 excercises

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -13,7 +13,13 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    match time_of_day {
+        0..=21 => Some(5),
+
+        22..=24 => Some(0),
+
+        _ => None,
+    }
 }
 
 #[cfg(test)]
@@ -34,6 +40,10 @@ mod tests {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+
+        match icecreams {
+            Some(value) => assert_eq!(value, 5),
+            None => assert!(false, "There should be icecream in the fridge at 12 PM"),
+        }
     }
 }
